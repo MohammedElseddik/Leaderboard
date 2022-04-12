d0fd2aa2aa251578341a9c8d1d59de363a81c4ae
Mohammed Elseddik Rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-04-12 22:41:12 +0100

    Add HTML markup

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
+
     entry: './src/index.js',
 
     plugins: [
