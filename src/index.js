36b86848b2f95497cd2491a729ad0ed1c77cc480
Mohammed Elseddik Rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-04-15 05:24:50 +0200

    Add postData that add names and scores to the game

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import './style.css';
-import { URL, gameId, scores, refreshBtn, submitBtn, fetchGameData } from './modules/API-services';
+import { URL, gameId, scores, refreshBtn, submitBtn, name, score, fetchGameData, postData } from './modules/API-services';
 
 
 
+
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    postData(URL, gameId, name.value, score.value);
+})
 
 refreshBtn.addEventListener('click', (event) => {
     event.preventDefault();
--- src/modules/API-services.js
+++ src/modules/API-services.js
@@ -1,6 +1,8 @@
 const scores = document.querySelector('.scores-section__result');
 const refreshBtn = document.querySelector('.refresh-btn');
 const submitBtn = document.querySelector('.submit-btn');
+const name = document.querySelector('.nameInput');
+const score = document.querySelector('.scoreInput');
 const URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
 const gameId = 'JXtYiaLm4miqbgKcY734';
 
@@ -15,7 +17,25 @@ const fetchGameData = async (url, gameId) => {
     } catch (error) {
         console.log(error);
     }
-
 }
 
-export { URL, gameId, scores, refreshBtn, submitBtn, fetchGameData };
+const postData = async (url, gameId, user, score) => {
+    try {
+        const data = await fetch(url + gameId + '/scores', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "user": `${user}`,
+                "score": score
+            }),
+        })
+        console.log('helo')
+        console.log(data)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export { URL, gameId, scores, refreshBtn, submitBtn, name, score, fetchGameData, postData };
